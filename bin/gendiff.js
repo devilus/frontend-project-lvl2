70b97cd657ffa26dccfdbce37eb861c61bd49675
Max Lyubimov <maxlubimov@gmail.com>
2022-04-02 18:40:33 +0300

    Add cli action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import { genDiff } from '../src/genDiff.js';
 
 const program = new Command();
 
@@ -8,6 +9,9 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .option('-f, --format <type>', 'output format')
-  .arguments('<filepath1>, <filepath2>');
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
 program.parse();
