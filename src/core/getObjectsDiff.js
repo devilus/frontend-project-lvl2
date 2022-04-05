e205f50ecc0a0edbb1db3ab306293572c777f2db
Max Lyubimov <maxlubimov@gmail.com>
2022-04-05 19:09:54 +0300

    Fix linter warnings & code refactoring

--- src/core/getObjectsDiff.js
+++ src/core/getObjectsDiff.js
@@ -1,42 +1,36 @@
 import _ from 'lodash';
 import { EOL } from 'os';
 
-const Sign = {
+const MathSign = {
   plus: '+',
   minus: '-',
-  nil: '',
 };
 
-const makeNewEntry = (key, value, sign) => {
+const makeNewEntry = (key, value, sign = '') => {
+  const indent = { big: 4, small: 2 };
+  const spacesCount = _.isEmpty(sign) ? indent.big : indent.small;
   const entry = `${sign} ${key}: ${value}`.trim();
-  const spaces = sign === Sign.nil ? 4 : 2;
-  return ' '.repeat(spaces) + entry;
+
+  return ' '.repeat(spacesCount) + entry;
 };
 
 const getObjectsDiff = (obj1, obj2) => {
   const keys = _.union(Object.keys(obj1), Object.keys(obj2)).sort();
 
-  const diff = keys.reduce((list, key) => {
+  const diff = keys.flatMap((key) => {
     const [value1, value2] = [_.get(obj1, key), _.get(obj2, key)];
-    const newEntry = [];
 
     if (_.has(obj1, key) && _.has(obj2, key)) {
-      value1 === value2
-        ? newEntry.push(makeNewEntry(key, value1, Sign.nil))
-        : newEntry.push(
-            makeNewEntry(key, value1, Sign.minus),
-            makeNewEntry(key, value2, Sign.plus)
-          );
-    } else {
-      _.isNil(value1)
-        ? newEntry.push(makeNewEntry(key, value2, Sign.plus))
-        : newEntry.push(makeNewEntry(key, value1, Sign.minus));
+      return value1 === value2
+        ? makeNewEntry(key, value1)
+        : [makeNewEntry(key, value1, MathSign.minus), makeNewEntry(key, value2, MathSign.plus)];
     }
 
-    return [...list, ...newEntry];
-  }, []);
+    // eslint-disable-next-line max-len
+    return _.isNil(value1) ? makeNewEntry(key, value2, MathSign.plus) : makeNewEntry(key, value1, MathSign.minus);
+  });
 
   return ['{', ...diff, '}'].join(EOL);
 };
 
-export { getObjectsDiff };
+export default getObjectsDiff;
