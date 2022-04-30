6bad2e86f89ffe769e37f37904533c4b4c49d0ba
Max Lyubimov <maxlubimov@gmail.com>
2022-04-30 09:16:13 +0300

    Add json style formatter

--- src/bundles.js
+++ src/bundles.js
@@ -2,6 +2,7 @@ import parseJsonFile from './parsers/parseJsonFile.js';
 import parseYamlFile from './parsers/parseYamlFile.js';
 import stylish from './formatters/stylish.js';
 import plain from './formatters/plain.js';
+import json from './formatters/json.js';
 
 export const Parsers = {
   json: parseJsonFile,
@@ -11,4 +12,5 @@ export const Parsers = {
 export const Formatters = {
   stylish,
   plain,
+  json,
 };
